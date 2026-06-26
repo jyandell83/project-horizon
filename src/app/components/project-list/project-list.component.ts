9679585f9559d4cfd2fef953a9eb1d6b35f3ffd9
Jeremy Yandell <jereyand@gmail.com>
2026-06-26 13:17:11 -0700

    displaying project names on project list component

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { PROJECTS } from '../../data/dummy-projects';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
   imports: [],
   templateUrl: './project-list.component.html',
-  styleUrl: './project-list.component.scss'
+  styleUrl: './project-list.component.scss',
 })
 export class ProjectListComponent {
-
+  projects: Project[] = PROJECTS;
 }
--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectListComponent } from '../../components/project-list/project-list.component';
 
 @Component({
   selector: 'app-projects-page',
   standalone: true,
-  imports: [],
+  imports: [ProjectListComponent],
   templateUrl: './projects-page.component.html',
-  styleUrl: './projects-page.component.scss'
+  styleUrl: './projects-page.component.scss',
 })
-export class ProjectsPageComponent {
-
-}
+export class ProjectsPageComponent {}

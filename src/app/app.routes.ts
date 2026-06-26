8138aadf125c796798078b9b607d91cbd3537f76
Jeremy Yandell <jereyand@gmail.com>
2026-06-26 13:09:05 -0700

    setting up routes and different page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { SessionsPageComponent } from './pages/sessions-page/sessions-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsPageComponent,
+  },
+  {
+    path: 'sessions',
+    component: SessionsPageComponent,
+  },
+];
---
+++ src/app/pages/sessions-page/sessions-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sessions-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './sessions-page.component.html',
+  styleUrl: './sessions-page.component.scss'
+})
+export class SessionsPageComponent {
+
+}

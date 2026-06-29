81a55c4b7cfd91b853315f9ace50fed9578dc0e1
Jeremy Yandell <jereyand@gmail.com>
2026-06-29 14:46:43 -0700

    adds some basic html and css for sessions page

--- src/app/components/session-list/session-list.component.ts
+++ src/app/components/session-list/session-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SESSIONS } from '../../data/dummy-sessions';
+import { ClimbingSession } from '../../models/session';
 
 @Component({
   selector: 'app-session-list',
   standalone: true,
   imports: [],
   templateUrl: './session-list.component.html',
-  styleUrl: './session-list.component.scss'
+  styleUrl: './session-list.component.scss',
 })
 export class SessionListComponent {
-
+  sessions: ClimbingSession[] = SESSIONS;
 }

02787a6c48821725ee65de07fa8e29253bd821ad
Jeremy Yandell <jereyand@gmail.com>
2026-06-29 14:53:39 -0700

    formats dates and times on session list, adds another dummy session

--- src/app/components/session-list/session-list.component.ts
+++ src/app/components/session-list/session-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { SESSIONS } from '../../data/dummy-sessions';
 import { ClimbingSession } from '../../models/session';
 
 @Component({
   selector: 'app-session-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './session-list.component.html',
   styleUrl: './session-list.component.scss',
 })
--- src/app/data/dummy-sessions.ts
+++ src/app/data/dummy-sessions.ts
@@ -31,4 +31,21 @@ export const SESSIONS: ClimbingSession[] = [
     notes:
       'Good session. Took enough time warming up before trying hard moves.',
   },
+  {
+    id: 1,
+    date: '2026-04-01',
+    location: 'Joshua Tree',
+    environment: 'outdoor',
+    startTime: '2026-06-28T18:00:00',
+    endTime: '2026-06-28T19:45:00',
+    phases: [
+      {
+        id: 4,
+        type: 'projecting',
+        startTime: '2026-06-28T18:45:00',
+        endTime: '2026-06-28T19:45:00',
+      },
+    ],
+    notes: 'Beautiful day, perfect weather for climbing',
+  },
 ];

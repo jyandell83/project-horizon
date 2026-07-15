7e11d45cb58f205eb1717bdae54fba3c4a97a533
Jeremy Yandell <jereyand@gmail.com>
2026-07-15 13:34:47 -0700

    add active session component

---
+++ src/app/components/active-session/active-session.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-session',
+  standalone: true,
+  imports: [],
+  templateUrl: './active-session.component.html',
+  styleUrl: './active-session.component.scss'
+})
+export class ActiveSessionComponent {
+
+}
--- src/app/pages/sessions-active-page/sessions-active-page.component.ts
+++ src/app/pages/sessions-active-page/sessions-active-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ActiveSessionComponent } from '../../components/active-session/active-session.component';
 
 @Component({
   selector: 'app-sessions-active-page',
   standalone: true,
-  imports: [],
+  imports: [ActiveSessionComponent],
   templateUrl: './sessions-active-page.component.html',
   styleUrl: './sessions-active-page.component.scss',
 })

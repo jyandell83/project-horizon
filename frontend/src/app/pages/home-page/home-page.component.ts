8a8878f56ea492019985d2ac10e7871d69cf505a
Jeremy Yandell <jereyand@gmail.com>
2026-09-08 12:00:42 -0700

    removes backend test from home page

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-home-page',
   standalone: true,
@@ -10,16 +8,4 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-  private http = inject(HttpClient);
-
-  backendMessage = '';
-
-  testBackend() {
-    this.http
-      .get<{ message: string }>('http://localhost:3000/api/hello')
-      .subscribe((response) => {
-        this.backendMessage = response.message;
-      });
-  }
-}
+export class HomePageComponent {}

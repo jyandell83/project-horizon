553606f18719367568a3e7d1bdcc67b24ae5d4cc
Jeremy Yandell <jereyand@gmail.com>
2026-09-15 21:49:48 -0700

    fix order of create table project_media

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -68,20 +68,7 @@ CREATE TABLE public.projects (
     user_id integer NOT NULL
 );
 
-CREATE TABLE project_media (
-  id SERIAL PRIMARY KEY,
-  project_id INTEGER NOT NULL
-    REFERENCES projects(id)
-    ON DELETE CASCADE,
 
-  media_type VARCHAR(20) NOT NULL DEFAULT 'image'
-    CHECK (media_type IN ('image', 'video')),
-
-  cloudinary_public_id TEXT NOT NULL,
-  url TEXT NOT NULL,
-  sort_order INTEGER NOT NULL DEFAULT 0,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 
 --
@@ -224,6 +211,21 @@ ALTER TABLE ONLY public.project_notes
 ALTER TABLE ONLY public.projects
     ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
 
+    
+CREATE TABLE project_media (
+  id SERIAL PRIMARY KEY,
+  project_id INTEGER NOT NULL
+    REFERENCES projects(id)
+    ON DELETE CASCADE,
+
+  media_type VARCHAR(20) NOT NULL DEFAULT 'image'
+    CHECK (media_type IN ('image', 'video')),
+
+  cloudinary_public_id TEXT NOT NULL,
+  url TEXT NOT NULL,
+  sort_order INTEGER NOT NULL DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 --
 -- Name: session_phases session_phases_pkey; Type: CONSTRAINT; Schema: public; Owner: -
